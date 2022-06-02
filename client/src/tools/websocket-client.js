29a7ac9b5e84b098a0e0171ebba66c303d816137
AndersMagnusson-AngryCreative <anders.magnusson@angrycreative.se>
2022-06-02 17:24:16 +0200

    reconect if websocket connection dies

--- client/src/tools/websocket-client.js
+++ client/src/tools/websocket-client.js
@@ -1,23 +1,45 @@
 const socketUrl = process.env?.NODE_ENV === "development" ? `ws://${window.location.hostname}:8080` : `ws://${window.location.hostname}:80/ws`;
 console.log(socketUrl)
-const socket = new WebSocket(socketUrl);
 
 export function wsInit(setAllScreenshots) {
-  socket.addEventListener("message", (event) => {
-    const response = JSON.parse(event.data);
-    console.log("Received message", response.action);
-    switch (response.action) {
-      case "CONNECTION":
-        console.log(response.data);
-        return;
+  let socket;
 
-      case "UPDATE_SCREENSHOTS":
-        console.log(response.data);
-        setAllScreenshots(response.data);
-        return;
-
-      default:
-        return;
+  function connect() {
+    if ( socket instanceof WebSocket ) {
+      socket.onerror = null;
+      socket.onclose = null;
+      socket.onopen = null;
+      socket.onmessage = null;
     }
-  });
+    socket = new WebSocket(socketUrl);
+
+    socket.onclose = () => {
+      console.log( 'socket closed. Trying to reconnect...' );
+      setTimeout( connect, 1000 );
+    };
+
+    socket.onmessage = (event) => {
+      const response = JSON.parse(event.data);
+      console.log("Received message", response.action);
+      switch (response.action) {
+        case "CONNECTION":
+          console.log(response.data);
+          return;
+
+        case "UPDATE_SCREENSHOTS":
+          console.log(response.data);
+          setAllScreenshots(response.data);
+          return;
+
+        default:
+          return;
+      }
+    };
+
+    console.log( socket );
+  }
+
+  if ( typeof socket === 'undefined' || socket.readyState === socket.CLOSED ) {
+    connect();
+  }
 }
