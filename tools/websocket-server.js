548d1351875e3517caa7d9733c560d6b53aef731
Jack Sandeman <jack.sandeman@angrycreative.uk>
2022-10-13 11:59:49 +0100

    refactored screenshot api code and added documentation

--- tools/websocket-server.js
+++ tools/websocket-server.js
@@ -5,6 +5,9 @@ const port = process.env.WEBSOCKET_PORT || 8080;
 logger.log("info", `Starting websocket server on port ${port}`);
 const wss = new WebSocketServer({ port });
 
+/**
+ * Initialises the websocket server
+ */
 function initWebSocket() {
   wss.on("connection", (ws) => {
     logger.log("debug", "New client connected with id");
@@ -14,6 +17,13 @@ function initWebSocket() {
   });
 }
 
+/**
+ * Sends a message to all connected clients
+ *
+ * @param action {String} - The action to perform
+ * @param data - The data to send
+ * @param sitePath - The site that the data affects
+ */
 function broadcastData(action, data, sitePath) {
   wss.clients.forEach((id, client) =>
     client.send(JSON.stringify({ action, data, sitePath }))
